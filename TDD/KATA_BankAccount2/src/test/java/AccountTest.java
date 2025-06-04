cf5a600b371d6351357eee6864827894a062c5af
nohog94 <nohog94@gmail.com>
2025-06-04 15:44:02 +0900

    vault backup: 2025-06-04 15:44:02

---
+++ TDD/KATA_BankAccount2/src/test/java/AccountTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void testAccount() {
+        Account account = new Account();
+        assertNull(account);
+
+    }
+}
